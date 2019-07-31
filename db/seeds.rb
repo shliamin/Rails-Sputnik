f3adf1f4d3aa0bfa2ec7d8d3f981df59f5539ecc
Efim Shliamin <shliamin@icloud.com>
2019-08-01 02:10:33 +0300

    working on logic & design, adding style and photos from db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,34 @@
 # require 'json'
 # require 'open-uri'
 
-url = 'https://api.sputnik8.com/v1/cities/2?api_key=db6584760c51a71d1c5124c95adb9829&username=shliamin@icloud.com'
-city_serialized = open(url).read
-city = JSON.parse(city_serialized)
 
-puts "Adding new city"
-# city["drinks"].each do |item|
-# # ingredient['drinks']
-# Ingredient.create(name: item["strIngredient1"])
-# end
+#Необходимо взять массив и для каждого элемента массива получить его id.
+#Это будет id каждго конкретного города, которое затем нужно будет с помощью
+#интерполяции вставить в url:
 
-City.create(name: city["name"], photo: city["geo"]["description"]["image"])
+#Миграция в базе данных не произойдет, если не очистить ее, так как
+#миграция не производится, если уже имеются одинаковые id.
+
+link_to_the_array = 'https://api.sputnik8.com/v1/cities?api_key=db6584760c51a71d1c5124c95adb9829&username=shliamin@icloud.com'
+cities_serialized = open(link_to_the_array).read
+cities_array = JSON.parse(cities_serialized)
+
+puts "Creating cities database"
+
+cities_array.each do |item|
+  url = "https://api.sputnik8.com/v1/cities/#{item["id"]}?api_key=db6584760c51a71d1c5124c95adb9829&username=shliamin@icloud.com"
+  city_serialized = open(url).read
+  city = JSON.parse(city_serialized)
+
+  City.create(id: item["id"], name: city["name"], photo: city["geo"]["description"]["image"])
+end
+
+
+# url = 'https://api.sputnik8.com/v1/cities/2?api_key=db6584760c51a71d1c5124c95adb9829&username=shliamin@icloud.com'
+# city_serialized = open(url).read
+# city = JSON.parse(city_serialized)
+
+# puts "Adding new city"
+
+
+# City.create(name: city["name"], photo: city["geo"]["description"]["image"])
