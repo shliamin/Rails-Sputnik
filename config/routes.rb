c9fbac1258721d0d471f782c0da37b0e82d4cfa7
Efim Shliamin <shliamin@icloud.com>
2019-07-31 10:39:53 +0300

    editing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :cities
+  resources :cities, except: :index
+
+  root to: 'cities#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
