f397fe3b784766d983eff8212f46285ce053fe67
Efim Shliamin <shliamin@icloud.com>
2019-08-01 03:29:44 +0300

    working on activities db seed, not ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 # require 'json'
 # require 'open-uri'
 
@@ -31,11 +27,4 @@ cities_array.each do |item|
 end
 
 
-# url = 'https://api.sputnik8.com/v1/cities/2?api_key=db6584760c51a71d1c5124c95adb9829&username=shliamin@icloud.com'
-# city_serialized = open(url).read
-# city = JSON.parse(city_serialized)
 
-# puts "Adding new city"
-
-
-# City.create(name: city["name"], photo: city["geo"]["description"]["image"])
