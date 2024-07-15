3587c696980a277dfd9b7a4c3b95705c77ebe542
Efim <efimshliamin@gmail.com>
2024-07-15 08:51:24 +0200

    migrations changed

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,5 +8,5 @@ class Activity < ApplicationRecord
   validates :description, presence: true
   validates :photo, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than или равен 5 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
