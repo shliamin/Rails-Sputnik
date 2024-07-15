8baf1f379d635103d2caaf065ed8f683b61af67d
Efim <efimshliamin@gmail.com>
2024-07-15 08:45:06 +0200

    migrations changed

--- db/migrate/20240715061430_add_devise_to_visitors.rb
+++ db/migrate/20240715061430_add_devise_to_visitors.rb
@@ -1,18 +1,16 @@
-# frozen_string_literal: true
-
 class AddDeviseToVisitors < ActiveRecord::Migration[6.1]
-  def self.up
-    change_table :visitors do |t|
+  def change
+    change_table :visitors, bulk: true do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "" unless column_exists?(:visitors, :email)
+      t.string :encrypted_password, null: false, default: "" unless column_exists?(:visitors, :encrypted_password)
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      t.string   :reset_password_token unless column_exists?(:visitors, :reset_password_token)
+      t.datetime :reset_password_sent_at unless column_exists?(:visitors, :reset_password_sent_at)
 
       ## Rememberable
-      t.datetime :remember_created_at
+      t.datetime :remember_created_at unless column_exists?(:visitors, :remember_created_at)
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -32,20 +30,12 @@ class AddDeviseToVisitors < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      t.timestamps null: false unless column_exists?(:visitors, :created_at)
     end
 
-    add_index :visitors, :email,                unique: true
-    add_index :visitors, :reset_password_token, unique: true
+    add_index :visitors, :email,                unique: true unless index_exists?(:visitors, :email)
+    add_index :visitors, :reset_password_token, unique: true unless index_exists?(:visitors, :reset_password_token)
     # add_index :visitors, :confirmation_token,   unique: true
     # add_index :visitors, :unlock_token,         unique: true
-  end
-
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
   end
 end
